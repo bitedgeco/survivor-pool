df2c0cb9962b8aede1eade2d311b16c5b71ae9a6
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-05 08:56:16 -0700

    basic file structure laid out

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'ipython',
+    'pyramid-ipython',
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    'pytest-watch',
+    'tox',
+]
+
+setup(name='survivor-pool',
+      version='0.1',
+      description='survivor-pool',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
+      author='James Canning, Zach Rickert, Derek Hewitt',
+      author_email=('info@bitedge.co,'
+                    'zachrickert@gmail.com,'
+                    'derekmhewitt@gmail.com',),
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      extras_require={
+          'testing': tests_require,
+      },
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = survivor_pool:main
+      [console_scripts]
+      init_db = survivor_pool.scripts.initializedb:main
+      """,
+      )
