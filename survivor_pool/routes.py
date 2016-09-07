4d1ae331f5d77923961474d31c605effdc25f496
zachrickert <zachrickert@gmail.com>
2016-09-06 22:13:01 -0700

    Made picks availible for each week.

--- survivor_pool/routes.py
+++ survivor_pool/routes.py
@@ -7,4 +7,5 @@ def includeme(config):
     config.add_route('logout', '/logout')
     config.add_route('pool', '/pool')
     config.add_route('pick', '/pick')
+    config.add_route('week', '/week/{week_num}')
     config.add_route('pick_test', '/pick_test')
