6868e7383a8672dddbd29588966e137f53adf8d8
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-07 10:20:33 -0700

    work on datetime script

---
+++ survivor_pool/scripts/helper.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import datetime
+
+"""This file contains helper functions that can be called by our site."""
+
+
+def find_current_week():
+    """This function returns the current week in the pool for which
+    selections can still be made."""
+    return current_week
