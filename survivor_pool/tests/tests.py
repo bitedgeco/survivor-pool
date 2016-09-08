53f95497bfba34a035de95476349248392a9bb89
Derek Hewitt <derekmhewitt@gmail.com>
2016-09-08 09:34:25 -0700

    more tests woo!  all passing

--- survivor_pool/tests/tests.py
+++ survivor_pool/tests/tests.py
@@ -88,8 +88,46 @@ def test_user_get_pick_for_week_error(testapp, new_session, populated_db):
     assert test_user._get_pick_for_week(11) == "User didn't pick a team for this week."
 
 
-def test_helper_find_current_week(testapp, new_session, populated_db):
-    """Test the scripts/helper.py function find_current_week."""
+def test_helper_find_current_week_fixed_date(testapp, new_session, populated_db):
+    """Test the scripts/helper.py function find_current_week
+    with a fixed input."""
     from ..scripts.helper import find_current_week
     current_time = datetime.datetime.strptime('Sunday September 09 2016 16:25', "%A %B %d %Y %H:%M")
     assert find_current_week(new_session, current_time) == 4
+
+
+def test_helper_find_current_week_no_input(testapp, new_session, populated_db):
+    """Test the scripts/helper.py function find_current_week with test data.
+
+    This test will naturally break in a few months and return None when the
+    season dates have all gone by."""
+    from ..scripts.helper import find_current_week
+    assert int(find_current_week(new_session))
+
+
+def test_helper_find_current_week_no_season_data(testapp, new_session):
+    """Test the scripts/helper.py function find_current_week with no db data.
+
+    This simulates being out of season and the function returning None."""
+    from ..scripts.helper import find_current_week
+    assert find_current_week(new_session) is None
+
+
+def test_security_check_credentials_pass(testapp, new_session, populated_db):
+    """Test security.py check_credentials function works properly."""
+    from ..security import check_credentials
+    assert check_credentials(new_session, "Bob Barker", "password") is True
+
+
+def test_security_check_credentials_fail(testapp, new_session, populated_db):
+    """Test security.py check_credentials function returns False for
+    an unauthorized user."""
+    from ..security import check_credentials
+    assert check_credentials(new_session, "Cris Ewing", "ice_cream") is False
+
+
+def test_security_check_credentials_error(testapp, new_session, populated_db):
+    """Test security.py check_credentials function except try:except block
+    passes a ValueError."""
+    from ..security import check_credentials
+    assert check_credentials(new_session, "Bob Barker", "") is False
