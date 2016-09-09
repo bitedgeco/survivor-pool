e1b44161498f3b0c9690c20d7912501b863d23dc
James <canning.jimmy@gmail.com>
2016-09-09 08:59:11 -0700

    bug fix

--- survivor_pool/scripts/initializedb.py
+++ survivor_pool/scripts/initializedb.py
@@ -37,8 +37,7 @@ def main(argv=sys.argv):
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
     settings = get_appsettings(config_uri, options=options)
-    # settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL', '')
-    settings['sqlalchemy.url'] = 'postgres://mskcipyyjpwgvd:qLKRNmDAUMG-aDT57Vk2_qTs9j@ec2-54-163-245-32.compute-1.amazonaws.com:5432/d9v1tjnd7rdc1b'
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL', '')
 
     engine = get_engine(settings)
     Base.metadata.drop_all(engine)
